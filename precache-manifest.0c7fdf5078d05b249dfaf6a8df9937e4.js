5ca0a2ec027c2a54413059692707ca8c147107ab
jain1601aman <54712796+jain1601aman@users.noreply.github.com>
2020-12-28 04:20:28 +0530

    Updates

---
+++ precache-manifest.0c7fdf5078d05b249dfaf6a8df9937e4.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3f66241075c666a92d92783d51a7d1",
+    "url": "/techlabs/index.html"
+  },
+  {
+    "revision": "6b681e8fd816cffb99da",
+    "url": "/techlabs/static/css/main.e1ecb597.chunk.css"
+  },
+  {
+    "revision": "e0971761f1ba89d19ce1",
+    "url": "/techlabs/static/js/2.c5b5fc04.chunk.js"
+  },
+  {
+    "revision": "7f5c15c882be7428c1c94cd76bcb513a",
+    "url": "/techlabs/static/js/2.c5b5fc04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b681e8fd816cffb99da",
+    "url": "/techlabs/static/js/main.2a93ec1e.chunk.js"
+  },
+  {
+    "revision": "5debbf6eaade43236045",
+    "url": "/techlabs/static/js/runtime-main.1a7302c2.js"
+  },
+  {
+    "revision": "6ea974ac889ccb72c01fa5ac8d33ede3",
+    "url": "/techlabs/static/media/1.6ea974ac.png"
+  },
+  {
+    "revision": "06be94201fae0a1bc1809037626175d4",
+    "url": "/techlabs/static/media/2.06be9420.png"
+  },
+  {
+    "revision": "e04b132b62fdb392efaa8fff0c0d2823",
+    "url": "/techlabs/static/media/3.e04b132b.png"
+  },
+  {
+    "revision": "c5a341ca7ec396d86afc291c0baec460",
+    "url": "/techlabs/static/media/4.c5a341ca.png"
+  },
+  {
+    "revision": "672d58b704aafac71010a7aed49625cf",
+    "url": "/techlabs/static/media/5.672d58b7.png"
+  },
+  {
+    "revision": "b05c093c64e255b13938fdeaf2dd4731",
+    "url": "/techlabs/static/media/6.b05c093c.png"
+  },
+  {
+    "revision": "6b2c82b933138c730774a30a90e2a3d0",
+    "url": "/techlabs/static/media/astronaut.6b2c82b9.svg"
+  },
+  {
+    "revision": "ec9c38dec5dfe466ee94e565e430a261",
+    "url": "/techlabs/static/media/earth.ec9c38de.svg"
+  },
+  {
+    "revision": "b6b4624e64c12e2060a50ada479ecb51",
+    "url": "/techlabs/static/media/home-hero.b6b4624e.jpg"
+  },
+  {
+    "revision": "0472a02f860f46f6bdf1be72ec79862f",
+    "url": "/techlabs/static/media/image404.0472a02f.svg"
+  },
+  {
+    "revision": "85016ec6282d84beefb410b2a9e42769",
+    "url": "/techlabs/static/media/img-1.85016ec6.jpg"
+  },
+  {
+    "revision": "e6733189f7f37de71b196aed3cea543d",
+    "url": "/techlabs/static/media/img-2.e6733189.jpg"
+  },
+  {
+    "revision": "80c7f9c7968a95ddb02e66cec2ed52e9",
+    "url": "/techlabs/static/media/img-3.80c7f9c7.jpg"
+  },
+  {
+    "revision": "767df8a1e2a7ccd890e339040a778314",
+    "url": "/techlabs/static/media/img-4.767df8a1.jpg"
+  },
+  {
+    "revision": "087668a4496e9d3a7defd04e24c2f9c1",
+    "url": "/techlabs/static/media/img-5.087668a4.jpg"
+  },
+  {
+    "revision": "f45c9eebbf8999159d0bfd9141fb9218",
+    "url": "/techlabs/static/media/logo.f45c9eeb.png"
+  },
+  {
+    "revision": "9fae76456c1a671601cb47417f978582",
+    "url": "/techlabs/static/media/moon.9fae7645.svg"
+  },
+  {
+    "revision": "0390ba854f8b9bfcb7ad1bc15b3fffed",
+    "url": "/techlabs/static/media/overlay_stars.0390ba85.svg"
+  },
+  {
+    "revision": "7461c5acbe983fb4c6306a64da2d976a",
+    "url": "/techlabs/static/media/rocket.7461c5ac.svg"
+  }
+]);
